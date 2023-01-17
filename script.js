678a21a102df770f82168facec12a98c5b10e11c
Mitchell-Steven <43509399+Mitchell-Steven@users.noreply.github.com>
2023-01-16 22:44:37 -0500

    Add files via upload
    Added options to play the game on easy, normal or hard (hard is a WIP)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**
+ * Array Variables
+ */
 //Array representing the cells on the board
 const gameCells = [
     document.getElementById("b0"),
@@ -21,14 +24,24 @@ const winCons = [
     [0, 4, 8],
     [2, 4, 6]
 ]
-
 var playerSelections = ["", "", "", "", "", "", "", "", ""] //Array that holds all cell selections made by the player and the computer
+var availableCells = [] //Array of cells available for the computer to place an O to attempt a win
+
+//Standard variables
 var currentPlayer = "X";    //Variable representing the current player (will be changed after each selection a player makes)
 var gameOver = false;   //Boolean that will be used to determine whether or not the game has ended
+var difficulty = "";    //The difficulty the player has selected the game to be
+var ranNum = 0; //Will hold the randomly generated number the computer will use to determine where to place an O
+var validPlay = false;  //Determines if the computer has selected a valid space for play
+
+disableButtons();
+
+/**
+ * THIS FUNCTION IS FOR THE ACTUAL GAME LOOP
+ */
 
 //Parameters are the id of the buttons the function is being used for and the indices of the playerSelection array
 function cellClick(id, pSelect) {
-
     /**
      * Tells the player to select play again if the game has ended and they attempt to click another cell
      * Gives the player an error if they attempt to click a cell that already contains an X or an O
@@ -46,9 +59,7 @@ function cellClick(id, pSelect) {
     else if (currentPlayer == "X")
     {
         document.getElementById(id).innerHTML = "X";
-        gameCells.forEach(element => {
-            element.disabled = true;
-        });
+        disableButtons();
         playerSelections[pSelect] = currentPlayer;
         document.getElementById("messageHeader").innerHTML = "";
         gameStatus();
@@ -64,13 +75,42 @@ function cellClick(id, pSelect) {
      * If a square already containing an X or an O is selected the loop will be run again with a different randomly selected square until a valid choice is found
      * Once a valid square is selected and the O is placed the player will be set to X and the loop will end
      */
-    if (currentPlayer == "O" && gameOver != true)
+    //Gameplay loop for when the user selects easy difficulty
+    if (currentPlayer == "O" && gameOver != true && difficulty == "easy")
     {
-        let validPlay = false;
+        validPlay = false;
 
         while (validPlay == false)
         {
-            let ranNum = Math.floor(Math.random() * 9);
+            ranNum = Math.floor(Math.random() * 9);
+
+            if (document.getElementById("b" + ranNum).innerHTML == "")
+            {
+                //console.log("I got here");
+                setTimeout(() => {if(currentPlayer == "O"){document.getElementById("b" + ranNum).innerHTML = "O"}}, 1000);
+                setTimeout(() => {enableButtons()}, 1000);
+                playerSelections[ranNum] = currentPlayer; 
+                gameStatus();
+                if (gameOver != true){
+                    setTimeout(() => {currentPlayer = "X"}, 1000);
+                }
+                validPlay = true;
+            }
+            else
+            {
+                //console.log("I did it");
+                continue;
+            }
+        }
+    }
+    //Gameplay loop for when the user selects normal difficulty
+    else if (currentPlayer == "O" && gameOver != true && difficulty == "normal")
+    {
+        validPlay = false;
+
+        while (validPlay == false)
+        {
+            ranNum = Math.floor(Math.random() * 9);
             
             for (let i = 0; i < winCons.length; i++)
             {
@@ -102,10 +142,84 @@ function cellClick(id, pSelect) {
             if (document.getElementById("b" + ranNum).innerHTML == "")
             {
                 setTimeout(() => {if(currentPlayer == "O"){document.getElementById("b" + ranNum).innerHTML = "O"}}, 1000);
-                setTimeout(() => {gameCells.forEach(element => {
-                    element.disabled = false;
-                })}, 1000);
-                playerSelections[ranNum] = currentPlayer;
+                setTimeout(() => {enableButtons()}, 1000);
+                playerSelections[ranNum] = currentPlayer; 
+                gameStatus();
+                if (gameOver != true){
+                    setTimeout(() => {currentPlayer = "X"}, 1000);
+                }
+                validPlay = true;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    //Gameplay loop for when the user selects hard
+    else
+    {
+        validPlay = false;
+
+        while (validPlay == false)
+        {
+            for (let i = 0; i < winCons.length; i++)
+            {
+                if (playerSelections[winCons[i][0]] == "O" && playerSelections[winCons[i][1]] == "O" || playerSelections[winCons[i][1]] == "O" && playerSelections[winCons[i][2]] == "O" || playerSelections[winCons[i][0]] == "O" && playerSelections[winCons[i][2]] == "O")
+                {
+                    for (let c = 0; c < winCons[i].length; c++)
+                    {
+                        if (playerSelections[winCons[i][c]] == "")
+                        {
+                            ranNum = winCons[i][c];
+                            break;
+                        }
+                    }
+                }
+                else if (playerSelections[winCons[i][0]] == "X" && playerSelections[winCons[i][1]] == "X" || playerSelections[winCons[i][1]] == "X" && playerSelections[winCons[i][2]] == "X" || playerSelections[winCons[i][0]] == "X" && playerSelections[winCons[i][2]] == "X")
+                {
+
+                    for (let d = 0; d < winCons[i].length; d++)
+                    {
+                        if (playerSelections[winCons[i][d]] == "")
+                        {
+                            ranNum = winCons[i][d];
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (playerSelections[winCons[i][0]] == "O" || playerSelections[winCons[i][1]] == "O" || playerSelections[winCons[i][2]] == "O")
+                    {
+                        if (playerSelections[winCons[i][0]] == "X" || playerSelections[winCons[i][1]] == "X" || playerSelections[winCons[i][2]] == "X")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            console.log("I got here!!!");
+
+                            if (playerSelections[winCons[i][0]] == "") {availableCells.push(playerSelections[winCons[i][0]])}
+
+                            if (playerSelections[winCons[i][1] == ""]) {availableCells.push(playerSelections[winCons[i][1]])}
+
+                            if (playerSelections[winCons[i][2]] == "") {availableCells.push(playerSelections[winCons[i][2]])}
+                        }
+                    }
+                }
+
+                console.log(availableCells);
+            }
+
+            ranNum = Math.floor(Math.random() * availableCells.length + 1);
+            console.log(ranNum);
+
+            if (document.getElementById("b" + ranNum).innerHTML == "")
+            {
+                setTimeout(() => {if(currentPlayer == "O"){document.getElementById("b" + ranNum).innerHTML = "O"}}, 1000);
+                setTimeout(() => {enableButtons()}, 1000);
+                playerSelections[ranNum] = currentPlayer; 
                 gameStatus();
                 if (gameOver != true){
                     setTimeout(() => {currentPlayer = "X"}, 1000);
@@ -121,6 +235,10 @@ function cellClick(id, pSelect) {
 }
 
 /**
+ * THIS FUNCTION IS ONLY FOR RESTARTING THE GAME
+ */
+
+/**
  * Runs when the player clicks play again
  * Foreach loop enables all the cells on the board, removes all the Xs and Os, resets the cell colours to their default
  * Removes error/victory/draw message from the screen if there is one, resets the player to X, resets the array containing player selections
@@ -128,7 +246,7 @@ function cellClick(id, pSelect) {
  */
 function playAgain() {
     gameCells.forEach(element => {
-        element.disabled = false;
+        element.disabled = true;
         element.innerHTML = "";
         element.style.backgroundColor=null;
     });
@@ -137,7 +255,15 @@ function playAgain() {
     playerSelections = ["", "", "", "", "", "", "", "", ""];
     gameOver = false;
     document.getElementById("replayHeader").innerHTML = "";
+    difficulty = "";
+    document.getElementById("easy").style.display = "inline-block";
+    document.getElementById("normal").style.display = "inline-block";
+    document.getElementById("hard").style.display = "inline-block";
 }
+
+/**
+ * ALL FUNCTIONS BELOW HERE ARE ONLY TO HELP THE GAME LOOP FUNCTION APPROPRIATELY
+ */
 
 /**
  * Checks if a player has won the game and if one has a message is displayed declaring the victor, the winning cells are hilighted green and the game is made inactive
@@ -160,9 +286,7 @@ function gameStatus() {
                 document.getElementById("b" + winCons[i][0]).style.backgroundColor="green";
                 document.getElementById("b" + winCons[i][1]).style.backgroundColor="green";
                 document.getElementById("b" + winCons[i][2]).style.backgroundColor="green";
-                gameCells.forEach(element => {
-                    element.disabled = false;
-                });
+                enableButtons();
                 gameOver = true;
             }
             
@@ -178,6 +302,27 @@ function gameStatus() {
             document.getElementById("b7").style.backgroundColor = "orange";
             document.getElementById("b8").style.backgroundColor = "orange";
             gameOver = true;
+            enableButtons();
         }
     }
+}
+
+function disableButtons() {
+    gameCells.forEach(element => {
+        element.disabled = true;
+    });
+}
+
+function enableButtons() {
+    gameCells.forEach(element => {
+        element.disabled = false;
+    });
+}
+
+function difficultySelect(id) {
+    difficulty = id;
+    document.getElementById("easy").style.display = "none";
+    document.getElementById("normal").style.display = "none";
+    document.getElementById("hard").style.display = "none";
+    enableButtons();
 }
